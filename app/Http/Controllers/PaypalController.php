36a7a5e90efccb6c49f8f4a3b5a03cc11a15081f
websmarts <iwmaclagan@bigpond.com>
2017-11-29 14:52:04 +1100

    added paypal support package and controller

---
+++ app/Http/Controllers/PaypalController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Fahim\PaypalIPN\PaypalIPNListener;
+
+class PaypalController extends Controller
+{
+
+    public function ipn()
+    {
+        $ipn = new PaypalIPNListener();
+        $ipn->use_sandbox = true;
+
+        $verified = $ipn->processIpn();
+
+        $report = $ipn->getTextReport();
+
+        Log::info("-----new payment-----");
+
+        Log::info($report);
+
+        if ($verified) {
+            if ($_POST['address_status'] == 'confirmed') {
+                // Check outh POST variable and insert your logic here
+                Log::info("payment verified and inserted to db");
+            }
+        } else {
+            Log::info("Some thing went wrong in the payment !");
+        }
+    }
+
+}
