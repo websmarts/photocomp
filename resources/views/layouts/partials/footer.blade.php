e5b40df81b568f56109890e0b5461e60a6c170ac
websmarts <iwmaclagan@bigpond.com>
2018-01-15 10:30:48 +1100

    added hard link to category definitions in footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,8 @@
  <div class="container">
     <div class="row">
         <div class="col-md-12" style="padding:10px; text-align: center;border-top: 1px solid #ccc">
-        <p>The competition terms and conditions can be <a target="_blank" href="{{ $settings->terms_and_conditions_url }}">viewed here</a></p>
+
+        <p>Competition <a target="_blank" href="{{ $settings->terms_and_conditions_url }}">terms and conditions</a> and  <a href="http://www.warragulnational.org/category-definitions/" target="_blank" >category definitions</a></p>
      	</div>
      </div>
 </div>
