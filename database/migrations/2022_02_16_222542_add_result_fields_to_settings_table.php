102c426f827938f86045866ec6cbd73c0d7c3cbf
Ian Maclagan <iwmaclagan@gmail.com>
2022-03-03 11:15:18 +1100

    migration issue, default values added for report block text fields

--- database/migrations/2022_02_16_222542_add_result_fields_to_settings_table.php
+++ database/migrations/2022_02_16_222542_add_result_fields_to_settings_table.php
@@ -14,8 +14,8 @@ class AddResultFieldsToSettingsTable extends Migration
     public function up()
     {
         Schema::table('settings', function (Blueprint $table) {
-            $table->string('result_report_top_text_block')->default('Add your top block block of text for reports email');
-            $table->mediumText('result_report_main_text_block')->default('Add your main block of text here for reports email');
+            $table->string('result_report_top_text_block')->nullable();
+            $table->mediumText('result_report_main_text_block')->nullable();
         });
     }
 
