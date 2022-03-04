b6f50d357edb75e3ee90b51799e022380fd8dd86
Ian Maclagan <iwmaclagan@gmail.com>
2022-03-04 14:06:19 +1100

    updated app/console.kernel to run queue:work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,6 +36,10 @@ class Kernel extends ConsoleKernel
         // })->hourly();
 
         $schedule->command('users:purge')->hourly();
+
+        if (!$this->osProcessIsRunning('queue:work')) {
+            $schedule->command('queue:work')->everyMinute();
+        }
     }
 
     /**
@@ -48,5 +52,22 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    protected function osProcessIsRunning($needle)
+    {
+        // get process status. the "-ww"-option is important to get the full output!
+        exec('ps aux -ww', $process_status);
+
+        // search $needle in process status
+        $result = array_filter($process_status, function($var) use ($needle) {
+            return strpos($var, $needle);
+        });
+
+        // if the result is not empty, the needle exists in running processes
+        if (!empty($result)) {
+            return true;
+        }
+        return false;
     }
 }
